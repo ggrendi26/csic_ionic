025a6ae02660a6ce910df9ef7099f9560193d3d8
ggrendi26 <sakrailink@gmail.com>
2020-11-14 22:37:17 +0700

    halaman lock dan topop

---
+++ src/app/components/modal-lock-user/modal-lock-user.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-lock-user',
+  templateUrl: './modal-lock-user.component.html',
+  styleUrls: ['./modal-lock-user.component.scss'],
+})
+export class ModalLockUserComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
