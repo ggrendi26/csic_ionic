0007baba750c068b98f40a16b2679952c8e75d19
Viriya Lucky <viriyaluckyoo@gmail.com>
2020-11-15 23:13:50 +0700

    redirect home

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from  "@angular/router";
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private  authService:  AuthService, private  router:  Router) { }
 
   ngOnInit() {
   }
 
+  login(form){
+    this.router.navigateByUrl('/home');
+
+  }
 }
