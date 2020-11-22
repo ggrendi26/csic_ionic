7bf166892d6e99214e3530c193a9185a74bfd49e
ggrendi26 <sakrailink@gmail.com>
2020-11-23 00:24:37 +0700

    trying lock-user request

--- src/app/lock-user/lock-user.page.ts
+++ src/app/lock-user/lock-user.page.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController, NavController } from '@ionic/angular';
+import { AuthService } from '../services/auth.service';
+import { FirestoreService } from '../services/firestore.service';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireAuth } from '@angular/fire/auth';
 
 @Component({
   selector: 'app-lock-user',
@@ -6,10 +13,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lock-user.page.scss'],
 })
 export class LockUserPage implements OnInit {
+  public createLockForm: FormGroup;
+  private nama: any;
+  private user: any;
+  constructor(
+    private navCtrl: NavController,
+    private authService: AuthService,
+    private formBuilder: FormBuilder,
+    private  router:  Router,
+    private firestoreService : FirestoreService,
+    public loadingCtrl: LoadingController,
+    public firestore: AngularFireAuth
+  ) { 
 
-  constructor() { }
+  }
 
   ngOnInit() {
+    // this.authService.userDetails().subscribe(res=>{
+    //   this.nama = res.value.nama;
+    //   console.log(this.nama);
+    // }
+    //   )
+    this.user=this.authService.userDetails();
+    console.log(this.user);
+  }
+
+  async requestLock(){
+    
   }
 
 }
