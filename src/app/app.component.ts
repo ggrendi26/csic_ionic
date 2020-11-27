8c3bc2b29355cda97ae1fe77b1a394de4532c8f5
vincent-limm <vlim231119@gmail.com>
2020-11-27 20:51:50 +0700

    role based sidemenu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
-import { NavController, Platform } from '@ionic/angular';
-import { SplashScreen } from '@ionic-native/splash-screen/ngx';
-import { StatusBar } from '@ionic-native/status-bar/ngx';
-import { AuthService } from './services/auth.service';
-import { Router } from '@angular/router';
-import { FirestoreService } from './services/firestore.service';
+import { NavController, Platform } from "@ionic/angular";
+import { SplashScreen } from "@ionic-native/splash-screen/ngx";
+import { StatusBar } from "@ionic-native/status-bar/ngx";
+import { AuthService } from "./services/auth.service";
+import { Router } from "@angular/router";
+import { FirestoreService } from "./services/firestore.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.scss']
+  selector: "app-root",
+  templateUrl: "app.component.html",
+  styleUrls: ["app.component.scss"],
 })
 export class AppComponent {
-  userKey: string; 
-  userName: string; 
+  userKey: string;
+  userName: string;
+  userRole: string;
   profile = null;
   profileImageUrl = "";
-  
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
@@ -36,45 +37,57 @@ export class AppComponent {
       this.splashScreen.hide();
     });
 
-    this.authSrv.userDetails().subscribe(res => {
-      if(res !== null){
-        this.userKey = res.uid;
+    this.authSrv.userDetails().subscribe(
+      (res) => {
+        if (res !== null) {
+          this.userKey = res.uid;
 
-        // get user details
-        this.firestoreService.getUserInfo(this.userKey).then((doc) => {
-          if(doc.exists){
-            // console.log(doc.data());
-            this.profile = doc.data();
-            this.userName = this.profile.nama;
-            // console.log(this.userName);
-            this.firestoreService.getProfileImageUrl(this.profile.profileImageUrl).then((res)=>{
-              this.profileImageUrl = res;
-              // console.log(res)
-            }).catch((error)=>{
-                console.log(error);
+          // get user details
+          this.firestoreService
+            .getUserInfo(this.userKey)
+            .then((doc) => {
+              if (doc.exists) {
+                // console.log(doc.data());
+                this.profile = doc.data();
+                this.userName = this.profile.nama;
+                this.userRole = this.profile.role;
+                // console.log(this.userName);
+                this.firestoreService
+                  .getProfileImageUrl(this.profile.profileImageUrl)
+                  .then((res) => {
+                    this.profileImageUrl = res;
+                    // console.log(res)
+                  })
+                  .catch((error) => {
+                    console.log(error);
+                  });
+              } else {
+                console.log("error getting document", doc);
+              }
+            })
+            .catch(function (error) {
+              console.log("error getting document", error);
             });
-          }else{
-            console.log('error getting document', doc)
-          }
-        }).catch(function (error){
-          console.log('error getting document', error)
-        });
-
-      } else {
-        this.userKey = '';
+        } else {
+          this.userKey = "";
+        }
+      },
+      (err) => {
+        console.log(err);
+        // this.router.navigateByUrl('/login');
       }
-    }, err => {
-      console.log(err);
-      // this.router.navigateByUrl('/login');
-    });
+    );
   }
 
-  LogoutUser(){
-    this.authSrv.logoutUser().then(res => {
-      // console.log(res);
-      this.navCtrl.navigateBack('');
-    }).catch(error => {
-      console.log(error);
-    });
+  LogoutUser() {
+    this.authSrv
+      .logoutUser()
+      .then((res) => {
+        // console.log(res);
+        this.navCtrl.navigateBack("");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 }
