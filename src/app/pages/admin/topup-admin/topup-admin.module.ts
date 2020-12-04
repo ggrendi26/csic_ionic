8565434b7acf6daacee999816a8757a50132e663
vincent-limm <vlim231119@gmail.com>
2020-12-04 22:30:44 +0700

    topup from admin done

--- src/app/pages/admin/topup-admin/topup-admin.module.ts
+++ src/app/pages/admin/topup-admin/topup-admin.module.ts
@@ -1,20 +1,21 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 
-import { IonicModule } from '@ionic/angular';
+import { IonicModule } from "@ionic/angular";
 
-import { TopupAdminPageRoutingModule } from './topup-admin-routing.module';
+import { TopupAdminPageRoutingModule } from "./topup-admin-routing.module";
 
-import { TopupAdminPage } from './topup-admin.page';
+import { TopupAdminPage } from "./topup-admin.page";
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TopupAdminPageRoutingModule
+    TopupAdminPageRoutingModule,
+    ReactiveFormsModule,
   ],
-  declarations: [TopupAdminPage]
+  declarations: [TopupAdminPage],
 })
 export class TopupAdminPageModule {}
--- src/app/pages/admin/topup-admin/topup-admin.page.ts
+++ src/app/pages/admin/topup-admin/topup-admin.page.ts
@@ -1,15 +1,90 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AngularFirestore } from "@angular/fire/firestore";
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from "@angular/forms";
+import { Router } from "@angular/router";
+import { LoadingController, ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-topup-admin',
-  templateUrl: './topup-admin.page.html',
-  styleUrls: ['./topup-admin.page.scss'],
+  selector: "app-topup-admin",
+  templateUrl: "./topup-admin.page.html",
+  styleUrls: ["./topup-admin.page.scss"],
 })
 export class TopupAdminPage implements OnInit {
-
-  constructor() { }
+  addTopUpAdmin: FormGroup;
+  validation_messages = {
+    nomorVA: [
+      { type: "required", message: "Virtual Account Number is required." },
+    ],
+    totalTopUp: [{ type: "required", message: "Total Topup is required." }],
+  };
+  constructor(
+    private formBuilder: FormBuilder,
+    private firestore: AngularFirestore,
+    public loadingCtrl: LoadingController,
+    private router: Router,
+    private toastCtrl: ToastController
+  ) {}
 
   ngOnInit() {
+    this.addTopUpAdmin = this.formBuilder.group({
+      nomorVA: new FormControl("", Validators.compose([Validators.required])),
+      totalTopUp: new FormControl(
+        "",
+        Validators.compose([Validators.required])
+      ),
+    });
   }
 
+  async tryTopUp(value) {
+    if (value) {
+      //   db.collection("cities").get().then(function(querySnapshot) {
+      //     querySnapshot.forEach(function(doc) {
+      //         doc.ref.update({
+      //             capital: true
+      //         });
+      //     });
+      // });
+      this.firestore
+        .collection("users", (ref) =>
+          ref.where("virtualAccount", "==", value.nomorVA)
+        )
+        .ref.get()
+        .then(function (querySnapshot) {
+          querySnapshot.forEach(function (doc) {
+            doc.ref.update({
+              saldo: doc.data()["saldo"] + value.totalTopUp,
+            });
+          });
+        });
+      this.presentToast();
+      this.router.navigateByUrl("/home-admin");
+    } else {
+      (error) => {
+        this.presentToastError();
+      };
+    }
+  }
+  async presentToast() {
+    let toast = this.toastCtrl.create({
+      message: "TopUp success!",
+      duration: 2000,
+      position: "bottom",
+    });
+
+    (await toast).present();
+  }
+  async presentToastError() {
+    let toast = this.toastCtrl.create({
+      message: "TopUp error!",
+      duration: 2000,
+      position: "bottom",
+    });
+
+    (await toast).present();
+  }
 }
