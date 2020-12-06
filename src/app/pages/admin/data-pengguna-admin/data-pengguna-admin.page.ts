5d1cb42da3db97248d73a0466ffafaa5b3a1800f
vincent-limm <vlim231119@gmail.com>
2020-12-07 00:49:43 +0700

    search user by name datapengguna admin

--- src/app/pages/admin/data-pengguna-admin/data-pengguna-admin.page.ts
+++ src/app/pages/admin/data-pengguna-admin/data-pengguna-admin.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from "@angular/core";
+import { FormControl } from "@angular/forms";
 import { FirestoreService } from "src/app/services/firestore.service";
+import { debounceTime } from "rxjs/operators";
 
 @Component({
   selector: "app-data-pengguna-admin",
@@ -9,12 +11,30 @@ import { FirestoreService } from "src/app/services/firestore.service";
 export class DataPenggunaAdminPage implements OnInit {
   allUser: any;
   userPic: any;
-  constructor(private firestoreService: FirestoreService) {}
+  backupUser: any = [];
+  searchNama: string = "";
 
-  ngOnInit() {
+  constructor(private firestoreService: FirestoreService) {
     this.firestoreService.getAllDocuments().then((data) => {
-      console.log(data);
       this.allUser = data;
+      this.backupUser = this.allUser;
+    });
+  }
+
+  ngOnInit() {}
+
+  onChange(event) {
+    const filteration = event.target.value;
+    this.allUser = this.filterItems(filteration);
+    if (filteration.length === 0) {
+      this.allUser = this.backupUser;
+    }
+  }
+
+  filterItems(searchNama) {
+    console.log(this.allUser);
+    return this.allUser.filter((user) => {
+      return user.nama.toLowerCase().indexOf(searchNama.toLowerCase()) > -1;
     });
   }
 }
