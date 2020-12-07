3e48d89de1fc0c487659a5343bffe619e79a2a6f
vincent-limm <vlim231119@gmail.com>
2020-12-07 09:09:43 +0700

    filter date done

--- src/app/pages/admin/data-pengguna-admin/data-pengguna-admin.page.ts
+++ src/app/pages/admin/data-pengguna-admin/data-pengguna-admin.page.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormControl } from "@angular/forms";
 import { FirestoreService } from "src/app/services/firestore.service";
-import { debounceTime } from "rxjs/operators";
+import { isBefore, isWithinInterval } from "date-fns";
+import { ToastController } from "@ionic/angular";
 
 @Component({
   selector: "app-data-pengguna-admin",
@@ -12,12 +13,20 @@ export class DataPenggunaAdminPage implements OnInit {
   allUser: any;
   userPic: any;
   backupUser: any = [];
+  filtered: any = [];
   searchNama: string = "";
+  startDate;
+  endDate;
+  invalidSelection: boolean = false;
 
-  constructor(private firestoreService: FirestoreService) {
+  constructor(
+    private firestoreService: FirestoreService,
+    private toastCtrl: ToastController
+  ) {
     this.firestoreService.getAllDocuments().then((data) => {
       this.allUser = data;
       this.backupUser = this.allUser;
+      this.filtered = this.allUser;
     });
   }
 
@@ -36,5 +45,36 @@ export class DataPenggunaAdminPage implements OnInit {
     return this.allUser.filter((user) => {
       return user.nama.toLowerCase().indexOf(searchNama.toLowerCase()) > -1;
     });
+  }
+
+  loadResults() {
+    if (!this.startDate || !this.endDate) {
+      return;
+    }
+    if (isBefore(this.endDate, this.startDate)) {
+      this.presentToastError();
+      this.allUser = this.backupUser;
+      this.invalidSelection = true;
+    }
+
+    const startDate = new Date(this.startDate);
+    const endDate = new Date(this.endDate);
+    this.allUser = this.allUser.filter((item) => {
+      return isWithinInterval(new Date(item.dateJoined), {
+        start: startDate,
+        end: endDate,
+      });
+    });
+    this.invalidSelection = false;
+  }
+
+  async presentToastError() {
+    let toast = this.toastCtrl.create({
+      message: "Invalid date.",
+      duration: 2000,
+      position: "bottom",
+    });
+
+    (await toast).present();
   }
 }
