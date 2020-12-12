6b661516c82e28fe4ab7e59b04826a4bf4fd55bd
vincent-limm <vlim231119@gmail.com>
2020-12-12 23:45:57 +0700

    log lock admin, home admin

--- src/app/pages/admin/home-admin/home-admin.page.ts
+++ src/app/pages/admin/home-admin/home-admin.page.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FirestoreService } from "src/app/services/firestore.service";
 
 @Component({
-  selector: 'app-home-admin',
-  templateUrl: './home-admin.page.html',
-  styleUrls: ['./home-admin.page.scss'],
+  selector: "app-home-admin",
+  templateUrl: "./home-admin.page.html",
+  styleUrls: ["./home-admin.page.scss"],
 })
 export class HomeAdminPage implements OnInit {
+  allUser: any;
+  allLock: any;
 
-  constructor() { }
+  constructor(private firestoreService: FirestoreService) {}
 
   ngOnInit() {
-  }
+    this.firestoreService.getAllDocuments().then((data) => {
+      this.allUser = data;
+    });
 
+    this.firestoreService.getAllLockDatas().then((data) => {
+      this.allLock = data;
+    });
+  }
 }
--- src/app/pages/admin/log-lock-admin/log-lock-admin.page.ts
+++ src/app/pages/admin/log-lock-admin/log-lock-admin.page.ts
@@ -1,15 +1,63 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { FirestoreService } from "src/app/services/firestore.service";
+import { isBefore, isWithinInterval } from "date-fns";
+import { ToastController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-log-lock-admin',
-  templateUrl: './log-lock-admin.page.html',
-  styleUrls: ['./log-lock-admin.page.scss'],
+  selector: "app-log-lock-admin",
+  templateUrl: "./log-lock-admin.page.html",
+  styleUrls: ["./log-lock-admin.page.scss"],
 })
 export class LogLockAdminPage implements OnInit {
-
-  constructor() { }
+  allLock: any;
+  backupLock: any;
+  startDate;
+  endDate;
+  invalidSelection: boolean = false;
+  constructor(
+    private firestoreService: FirestoreService,
+    private toastCtrl: ToastController
+  ) {}
 
   ngOnInit() {
-  }
+    this.firestoreService
+      .getAllLockDatas()
+      .then((docs) => {
+        this.allLock = docs;
+        this.backupLock = this.allLock;
+        // console.log(this.lockDatas);
+      })
+      .catch(function (error) {});
 
+    console.log();
+  }
+  loadResults() {
+    if (!this.startDate || !this.endDate) {
+      return;
+    }
+    if (isBefore(new Date(this.endDate), new Date(this.startDate))) {
+      this.presentToastError();
+      this.allLock = this.backupLock;
+      this.invalidSelection = true;
+    }
+
+    const startDate = new Date(this.startDate);
+    const endDate = new Date(this.endDate);
+    this.allLock = this.backupLock.filter((item) => {
+      return isWithinInterval(new Date(item.dateLock), {
+        start: startDate,
+        end: endDate,
+      });
+    });
+    this.invalidSelection = false;
+  }
+  async presentToastError() {
+    let toast = this.toastCtrl.create({
+      message: "Invalid date.",
+      duration: 2000,
+      position: "bottom",
+    });
+
+    (await toast).present();
+  }
 }
